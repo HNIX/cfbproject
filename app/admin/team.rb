35c279fe5b10c10032e03ea66195b838e85291bb
Prelang Builder <builder@prelang.com>
2015-08-06 00:49:23 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Team' to allow for create, update

--- app/admin/team.rb
+++ app/admin/team.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Team do
+  permit_params :name, :data_name
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
